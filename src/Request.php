8508fb0827ebd17047c23d394655d258e086d9a5
victo <victoryellow300@gmail.com>
2026-01-27 21:42:42 +0500

    Request class

---
+++ src/Request.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace Server;
+
+class Request
+{
+    public static function validate(string $request): bool
+    {
+        return true;
+    }
+}
--- src/Server.php
+++ src/Server.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Server;
 
+use Server\Request;
+
 class Server
 {
+    CONST READ_LENGTH = 1024;
     private \Socket $socket;
 
     public function __construct(public readonly string $address, public readonly int $port) 
@@ -15,9 +18,38 @@ class Server
         socket_bind($this->socket, $address, $port);
     }
 
-    public function listen() : void
+    public function listen() : never
     {
-        socket_listen($this->socket);
+        while (true)
+        {
+            socket_listen($this->socket);
+
+            $client = socket_accept($this->socket);
+
+            if (!$client) {
+                socket_close($client);
+                continue;
+            }
+
+            $source = socket_read($client, self::READ_LENGTH);
+
+            if (!Request::validate($source)) {
+                socket_close($client);
+                continue;
+            }
+
+            socket_write($client, "Hello from my server");
+
+            if (!$source) {
+                socket_close($client);
+                continue;
+            }
+
+            var_dump(strlen($source));
+            var_dump($source);
+
+            socket_close($client);
+        }
     }
 
     public function __destruct()
@@ -25,3 +57,7 @@ class Server
         socket_close($this->socket);
     }
 }
+
+$server = new Server("127.0.0.1", 80);
+
+$server->listen();
